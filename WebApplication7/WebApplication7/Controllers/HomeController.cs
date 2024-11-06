96136f9c21598cf2674d953dfffe0d126db73078
PonomarenkoDM <159808442+PonomarenkoDM@users.noreply.github.com>
2024-11-06 15:06:28 +0200

    Add files via upload

---
+++ WebApplication7/WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index(string name="Oleg", string color = "Black")
+        {
+            ViewBag.Name = name;
+            ViewBag.Color = color;
+
+            return View();
+        }
+    }
+}
